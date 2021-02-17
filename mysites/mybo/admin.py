13af7497a7a1547d6878410062ceb5d69942b949
tmdlucy0214 <73995690+tmdlucy0214@users.noreply.github.com>
2021-02-17 09:48:17 +0900

    Delete mysites directory

--- mysites/mybo/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Question
-
-# Register your models here.
-class QuestionAdmin(admin.ModelAdmin):
-    search_fields=['subject'] #질문 제목으로 검색
-
-admin.site.register(Question, QuestionAdmin)
